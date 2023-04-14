4b7d4c176631a807b8ed34abe0cba895daad2c65
Tristan Downing <downing.tristan@gmail.com>
2023-04-14 15:14:29 -0400

    core serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,33 @@
 from rest_framework import serializers
 
-from api.models import Model
+from api.models import Model, QualElement, QuantElement, Layout, QualElementPosition, QuantElementPosition, Connection
 
 
 class ModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Model
         fields = ["id", "label"]
+
+
+class LayoutSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Layout
+        fields = ["id", "label"]
+
+
+class QualElementSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = QualElement
+        fields = ["id", "label"]
+
+
+class QualElementPositionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = QualElementPosition
+        fields = ["id", "x", "y", "element_id", "layout_id"]
+
+
+class ConnectionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Connection
+        fields = ["id", "from_element_id", "to_element_id"]
